f23dad5eee3bfd624cbbf666522195002cf36f2f
gutche <gledrianregalagutierrez@gmail.com>
2023-09-17 19:26:04 +0200

    fix: project layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.js"],
   theme: {
+    screen: {
+      "2xs": "320px",
+      "xs": "375px",
+      "sm": "425px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1440px",
+      "2xl": "2560px"
+    },
     container: {
       center: true,
     },
